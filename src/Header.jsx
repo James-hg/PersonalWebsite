6fa95d2578159292e613f804f5406d4a621ecc59
James Hoang <jameshoang2304@gmail.com>
2024-12-11 17:50:28 -0800

    update website

---
+++ src/Header.jsx
@@ -0,0 +1,16 @@
+function Header() {
+    return (
+        <div className="nav-bar">
+            <div className="left-section">
+                <div className="home">JH</div>
+                <div className="separator">|</div>
+                <div className="title">Dashboard</div>
+            </div>
+            <div className="right-section">
+                <div className="profile">profile</div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
